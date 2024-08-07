e7b95dca910aa859a2eb796bd01b9817d69b6288
Asad Ali Asim <asadaliasim909@gmail.com>
2024-08-07 13:40:31 +0500

    feat:routes connected with dummy controllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,17 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  logoutUser,
+  registerUser,
+  updateUserProfile,
+} from '../controllers/userController.js';
 
+router.post('/', registerUser);
 router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+// chaining of multiple routes having different req methods
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
 export default router;
