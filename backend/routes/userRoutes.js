d090b19cced4faffcee91e356d22a0f9baecdb78
Asad Ali Asim <asadaliasim909@gmail.com>
2024-08-07 16:07:07 +0500

    feat:routes protected

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,11 +7,14 @@ import {
   registerUser,
   updateUserProfile,
 } from '../controllers/userController.js';
-
+import { protect } from '../middleware/authMiddleware.js';
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
 // chaining of multiple routes having different req methods
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
